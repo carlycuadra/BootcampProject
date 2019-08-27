1f584ddd768aa095efa59d44f80910bcfa71c5eb
carlycuadra <carlycuadra@ufl.edu>
2019-08-27 13:42:33 -0400

    finished

--- server.js
+++ server.js
@@ -1,51 +1,53 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    url = require('url'),
-    port = 8080;
+
+
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var port = 8080;
 
 /* Global variables */
-var listingData, server;
+var listingData,server;
+//test
+var requestHandler = function (request, response) {
+  console.log(parsedUrl);
+  console.log(request.method);
+var parsedUrl = url.parse(request.url);
 
-var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+if (request.url === "/listings") {
+response.statusCode = 200;
+response.write(listingData)
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+} else {
+
+
+response.statusCode = 404;
+
+response.write("Bad gateway error");
+
+}
+
+
+
+response.end();
+
 };
 
+//readFile is used to read the file in node.js
+
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-    HINT: Read up on JSON parsing Node.js
-   */
-
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
+  listingData=data;
+  // if (err) throw err;
+  server = http.createServer(requestHandler);
+  server.listen(port, function() {
 
 
+// 
+// // the server is now started, listening for requests on port 8080
+// 
+// //once the server is listening, this callback function is executed
+// //Start the server
+console.log('Server listening on: http://127.0.0.1:' + port);
+
+});  
 });
